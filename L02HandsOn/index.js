02230ec2d26af28d7be0591218ae8329d736e140
ajbernabeo <45241240+ajbernabeo@users.noreply.github.com>
2020-07-31 13:24:59 -0700

    Add files via upload

---
+++ L02HandsOn/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var Entry = /** @class */ (function () {
+    function Entry(firstName, lastName, birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+    }
+    return Entry;
+}());
+var newPerson = new Entry('Albert', 'Einstein', new Date('5/14/1879'));
+console.log("First Name: " + newPerson.firstName + " \nLast Name: " + newPerson.lastName + " \nBirthday: " + newPerson.birthday);
+//Part 2 =========================================================================================================================
+var numbers = ['twelve', 'one-hundred', 'sixteen', 'forty-four', 'seventy-three', 'three-million'];
+function sortArray(array) {
+    return array.sort();
+}
+console.log("Array before sorting: " + numbers);
+console.log("Array after sorting: " + sortArray(numbers));
