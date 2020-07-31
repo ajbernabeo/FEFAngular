bc5bb46bf0edc6276302d1a50e82a0cd6a1bbaa0
ajbernabeo <45241240+ajbernabeo@users.noreply.github.com>
2020-07-31 14:24:26 -0700

    Add files via upload

---
+++ L03HandsOn/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mr. Bigglesworth';
+  city = 'Anthioch, CA';
+  tagLine = 'If I fit, I sit';
+  aboutMe = 'Eat and sleep is what I do everyday. I am the master of this house. And if someone tries to take my crown, something really bad will happen to you. I don\'t know what it is, but something really bad will happen to you; unless you feed me'
+}
